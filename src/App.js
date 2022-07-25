88fb5f75654ced53c09daa24b65e5c913a1f1b1e
Facundo <facugastonbarral@gmail.com>
2022-07-25 10:48:56 -0300

    Add new routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import List from "./pages/List";
 import New from "./pages/New";
 import View from "./pages/View";
-
+import Code from "./pages/Code"
+import Edit from "./pages/Edit"
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="/lista" element={<List />} />
+          <Route path="/codigo" element={<Code />} />
           <Route path="/mascotas/crear" element={<New />} />
+          <Route path="/mascotas/editar/:id" element={<Edit />} />
           <Route path="/mascotas/:id" element={<View />} />
         </Routes>
       </BrowserRouter>
